3997b5872d42f3c720e065b33af3d7d7f59d6c53
essadany <essadanyya@cy-tech.fr>
2023-07-16 11:52:20 +0100

    filter histograms to show

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -36,8 +36,8 @@ export default function Dashboard() {
       .then((response) => response.json())
       .then((jsonData) => {
         // Process your JSON data to extract required information (e.g., salaries)
-        const numbers_of_claims = jsonData.map((user) => user.number_of_claims);
-        const labels = jsonData.map((user) => user.bontaz_part_number);
+        const numbers_of_claims = jsonData.map((item) => item.number_of_claims);
+        const labels = jsonData.map((item) => item.bontaz_part_number);
 
         // Prepare the chart data using the extracted salaries
         const chartData = {
@@ -313,12 +313,12 @@ export default function Dashboard() {
               week : week,
              shipped_parts : shippedParts,
              objectif: objectif,
-             ppm: ppm,
            }),
          })
          
          if (res.status === 200) {
            setShippedParts('');
+           setObjectif(3);
          } else {
            alert("Some error occured, try again!");
          }
@@ -326,91 +326,269 @@ export default function Dashboard() {
          console.log(err);
        }
      };
+    // Show Histogram of PPM---------------------------------------------------------------------------------------------------------------
+    const [year3, setYear3] = useState(2023);
+    const [data3, setData3] = useState(null);
+    
+    // French month names array
+    const weeks = [];
+    for (let week = 1; week <= 52; week++) {
+      weeks.push("S" + week);
+    }    
+    useEffect(() => {
+      // Fetch your JSON data
+      fetch(`http://127.0.0.1:8000/api/year/${year3}/week_ppm`)
+        .then((response) => response.json())
+        .then((jsonData3) => {
+          const filteredData5 = jsonData3.filter((item)=>item.week_ppm)
+          const ppm_values = weeks.map((week, index) => {
+            const weekData = filteredData5.find((item) => item.week === index + 1);
+            return weekData ? weekData.week_ppm : 0;
+          });
+          const filteredData6 = jsonData3.filter((item)=>item.objectif)
+          const objectif_values = weeks.map((week, index) => {
+            const weekData = filteredData5.find((item) => item.week === index + 1);
+            return weekData ? weekData.objectif : objectif;
+          });
+         
+          const chartData3 = {
+            labels: weeks,
+            datasets: [
+              {
+                label: 'ppm value',
+                data: ppm_values,
+                backgroundColor: 'blue',
+                barThickness: 15,
+                type : 'bar'
+              },
+              {
+                label: 'objectif value',
+                data: objectif_values,
+                backgroundColor: 'green',
+                fill : false,
+                type : 'line',
+
+              },
+            ],
+          };
+    
+          setData3(chartData3);
+        })
+        .catch((error) => {
+          console.error('Error fetching JSON data:', error);
+        });
+    }, [year3]);
+    
+
+    const options3 = {
+      scales: {
+        x: {
+          beginAtZero: true,
+          title: {
+          },
+        },
+        y: {
+          beginAtZero: true,
+          title: {
+          },
+          suggestedMax: 10,
+          suggestedMin: 0, // Set the minimum value on the Y-axis
+        },
+      },
+    };
+
+  // Show the choosen histogram------------------------------------------------------------------------------------------------------
+  const [display_css1, setDisplay_css1] = useState('disabled');
+  const [display_css2, setDisplay_css2] = useState('none');
+  const [display_css3, setDisplay_css3] = useState('none');
+  const [display_css4, setDisplay_css4] = useState('none');
+  const [histogram, setHistogram] = useState('1');
+  
+  /*const ShowHistogram = () => {
+    if (histogram === '1') {
+      setDisplay_css1('disabled');
+      setDisplay_css2('none');
+      setDisplay_css3('none');
+      setDisplay_css4('none');
+    } else if (histogram === '2') {
+      setDisplay_css1('none');
+      setDisplay_css2('disabled');
+      setDisplay_css3('none');
+      setDisplay_css4('none');
+    } else if (histogram === '3') {
+      setDisplay_css1('none');
+      setDisplay_css2('none');
+      setDisplay_css3('disabled');
+      setDisplay_css4('none');
+    } else if (histogram === '4') {
+      setDisplay_css1('none');
+      setDisplay_css2('none');
+      setDisplay_css3('none');
+      setDisplay_css4('disabled');
+    }
+  };*/
+
   return (
     <>
-        
-        <div className='main'>
-        <h2 >Dashboard</h2>
-        <div  className='border '>
-          <div className='ppm'>
-          <form style={{width:'500px'}} className='row container' onSubmit={handleSubmit}>
-          <div className='col-4'>
-            <label>Year:
-          <select className='selectpicker form-select form-select-sm' value={year} onChange={(e) => setYear(e.target.value)}>
-            <option value={new Date().getFullYear()}>{new Date().getFullYear()}</option>
-          </select></label>
+      <div className='main'>
+        <h2>Dashboard</h2>
+        <div className='border'>
+          <div>
+            <form>
+              <label>
+                Histogram:
+                <select
+                  className='selectpicker form-select form-select-sm'
+                 
+                  onChange={(e) => 
+                    setHistogram(e.target.value)
+                  }
+                >
+                  <option value='1'>TOP 5 PRODUCTS CLAIMED</option>
+                  <option value='2'>CLAIMS STATUS</option>
+                  <option value='3'>8D STATUS OF CONFIRMED CLAIMS</option>
+                  <option value='4'>PPM</option>
+                </select>
+              </label>
+            </form>
           </div>
-          <div className='col-4'>
-                <label>
-              Month:
-              <select className='selectpicker form-select form-select-sm' value={month} onChange={(e) => setMonth(e.target.value)}>
-                <option value={new Date().getMonth() + 1}>{new Date().toLocaleString('default', { month: 'long' })}</option>
+          {
+            histogram==='1'&&<div>
+            {data && <Bar width={500} height={200} data={data} options={options} />}
+          </div>
+          }
+          {
+            histogram==='2'&& <div>
+            <form>
+              <label>
+                Year:
+                <select
+                  className='selectpicker form-select form-select-sm'
+                  value={year1}
+                  onChange={(e) => setYear1(Number(e.target.value))}
+                >
+                  <option value={2023}>2023</option>
+                  <option value={2024}>2024</option>
+                  <option value={2025}>2025</option>
+                  <option value={2026}>2026</option>
+                </select>
+              </label>
+            </form>
+            {data1 && <Bar width={500} height={200} data={data1} options={options1} />}
+          </div>
+          }
+          {
+            histogram==='3' && <div>
+            <form>
+              <label>
+                Year:
+                <select
+                  className='selectpicker form-select form-select-sm'
+                  value={year2}
+                  onChange={(e) => setYear2(Number(e.target.value))}
+                >
+                  <option value={2023}>2023</option>
+                  <option value={2024}>2024</option>
+                  <option value={2025}>2025</option>
+                  <option value={2026}>2026</option>
+                </select>
+              </label>
+            </form>
+            {data2 && <Bar width={500} height={200} data={data2} options={options2} />}
+          </div>
+          }
+          {
+            histogram==='4' && <div>
+            <div className='ppm'>
+              <form style={{ width: '500px' }} className='row container' onSubmit={handleSubmit}>
+                <div className='col-4'>
+                  <label>
+                    Year:
+                    <select
+                      className='selectpicker form-select form-select-sm'
+                      value={year}
+                      onChange={(e) => setYear(e.target.value)}
+                    >
+                      <option value={new Date().getFullYear()}>{new Date().getFullYear()}</option>
+                    </select>
+                  </label>
+                </div>
+                <div className='col-4'>
+                  <label>
+                    Month:
+                    <select
+                      className='selectpicker form-select form-select-sm'
+                      value={month}
+                      onChange={(e) => setMonth(e.target.value)}
+                    >
+                      <option value={new Date().getMonth() + 1}>{new Date().toLocaleString('default', { month: 'long' })}</option>
+                    </select>
+                  </label>
+                </div>
+                <div className='col-4'>
+                  <label>
+                    Week:
+                    <select className='selectpicker form-select form-select-sm' value={week} onChange={(e) => setWeek(e.target.value)}>
+                      <option disabled={week === 1} value={getCurrentWeekNumber() - 1}>
+                        {getCurrentWeekNumber() - 1}
+                      </option>
+                      <option disabled={week === 1} value={getCurrentWeekNumber()}>
+                        {getCurrentWeekNumber()}
+                      </option>
+                    </select>
+                  </label>
+                </div>
+                <div className='col-4'>
+                  <label>
+                    Shipped Parts:
+                    <input
+                      type='number'
+                      className='form-control form-control-sm'
+                      value={shippedParts}
+                      onChange={(e) => setShippedParts(e.target.value)}
+                    />
+                  </label>
+                </div>
+                <div className='col-4'>
+                  <label>
+                    Objectif:
+                    <input
+                      type='number'
+                      className='form-control form-control-sm'
+                      value={objectif}
+                      onChange={(e) => setObjectif(e.target.value)}
+                    />
+                  </label>
+                </div>
+                <div className='col-4' style={{ textAlign: 'center' }}>
+                  <Button variant='success' className='btn btn-sm' type='submit'>
+                    Add PPM
+                  </Button>
+                </div>
+              </form>
+            </div>
+            <form>
+            <label>
+              Year:
+              <select
+                className='selectpicker form-select form-select-sm'
+                value={year3}
+                onChange={(e) => setYear3(Number(e.target.value))}
+              >
+                <option value={2023}>2023</option>
+                <option value={2024}>2024</option>
+                <option value={2025}>2025</option>
+                <option value={2026}>2026</option>
               </select>
             </label>
+          </form>
+          <div>{data3 && <Bar width={500} height={400} data={data3} options={options3} />}</div>
           </div>
-          <div className='col-4'>
-              <label>
-            Week :
-            <select className='selectpicker form-select form-select-sm' value={week} onChange={(e) => setWeek(e.target.value)}>
-            <option disabled={week===1}  value={getCurrentWeekNumber()-1}>{getCurrentWeekNumber()-1}</option>
-              <option disabled={week===1} value={getCurrentWeekNumber()}>{getCurrentWeekNumber()}</option>
-            </select>
-          </label>
-          </div>
-          <div className='col-4'>
-            <label>
-              Shipped Parts:
-              <input type="number" className="form-control form-control-sm" value={shippedParts} onChange={(e) => setShippedParts(e.target.value)} />
-            </label>
-          </div>
-          <div className='col-4'>
-          <label>
-            Objectif:
-            <input type="number" className="form-control form-control-sm" value={objectif} onChange={(e) => setObjectif(e.target.value)} />
-          </label>
-          </div>
-          <div className='col-4' style={{textAlign:'center'}}>
-          <Button variant='success' className='btn btn-sm' type="submit">Add PPM</Button>
-          </div>
-        
-    </form>
-          </div>
-        
-            <div>
-              {data && <Bar  width={500} height={400} data={data} options={options}/>}
-            </div>
-            <div>
-            <form>
-            <label>Year:
-              <select className='selectpicker form-select form-select-sm'  onChange={(e) => setYear1(Number(e.target.value))}>
-                <option selected  >2023</option>
-                <option  >2024</option>
-                <option  >2025</option>
-                <option  >2026</option>
-              </select></label>
-            </form>
-              {data1 && <Bar width={500} height={400}   data={data1} options={options1}/>}
-            </div><br />
-            <div>
-            <form>
-              <label>Year:
-                <select className='selectpicker form-select form-select-sm'  onChange={(e) => setYear2(Number(e.target.value))}>
-                  <option selected  >2023</option>
-                  <option  >2024</option>
-                  <option  >2025</option>
-                  <option  >2026</option>
-                </select></label>
-              </form>
-              {data2 && <Bar  width={500} height={400} data={data2} options={options2}/>}
-            </div>
-            <div>
-              {data && <Bar  width={500} height={400} data={data} options={options}/>}
-            </div>
-          </div>
+          }
+          
+          
         </div>
-        
-    
+      </div>
     </>
-    
-  )
+  );
 }
